0e2949f42aad70455b0a625c14bb2cd5eb7f8a62
Alex <alexdu74960@gmail.com>
2018-02-16 14:35:40 +0100

    Add files via upload
    Class Exemplaire

---
+++ src/projetBibliothéque/Exemplaire.java
@@ -0,0 +1,14 @@
+package gestiondocu;
+
+import java.util.Date;
+
+
+public class Exemplaire {
+    private int numEx;
+    private int dateRecep;
+    private boolean empruntable;
+    
+    Exemplaire(/*Ouvrave o,*/ int numEx, Date dateReception ){
+        
+    }
+}
